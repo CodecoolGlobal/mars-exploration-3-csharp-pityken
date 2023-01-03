ff5f4de571638fb97960befbf96e7dcc3afdc5d6
tercsd <tercsd@gmail.com>
2023-01-03 16:52:46 +0100

    add BuildableDeterminer interface

---
+++ Codecool.MarsExploration.MapExplorer/Exploration/Service/IBuildableDeterminer.cs
@@ -0,0 +1,8 @@
+﻿using Codecool.MarsExploration.MapExplorer.Exploration.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Exploration.Service;
+
+public interface IBuildableDeterminer
+{
+    bool Determine(SimulationContext simulationContext, string roverId);
+}
