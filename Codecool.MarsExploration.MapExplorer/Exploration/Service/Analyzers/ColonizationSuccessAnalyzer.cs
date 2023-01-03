36dcc65a97795b71fa0a38eadedb896cfabf4bb3
tercsd <tercsd@gmail.com>
2023-01-03 15:50:09 +0100

    add new colonization success condition and removed global timeout

---
+++ Codecool.MarsExploration.MapExplorer/Exploration/Service/Analyzers/ColonizationSuccessAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using Codecool.MarsExploration.MapExplorer.Exploration.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Exploration.Service.Analyzers;
+
+public class ColonizationSuccessAnalyzer : IAnalyzer
+{
+    public ExplorationOutcome Analyze(SimulationContext simulationContext)
+    {
+        if (CheckColonizableAmountsOfCommandCenters(simulationContext))
+        {
+            return ExplorationOutcome.Colonizable;
+        }
+        return ExplorationOutcome.None;
+    }
+
+    private static bool CheckColonizableAmountsOfCommandCenters(SimulationContext simulationContext)
+    {
+        int colonizableAmountOfCommandCenters = 3;
+
+        int amountOfWorkingCommandCenters = 0;
+        foreach (var commandCenter in simulationContext.CommandCenters)
+        {
+            if (commandCenter.CommandCenterStatus != CommandCenter.Model.CommandCenterStatus.UnderConstruction)
+                amountOfWorkingCommandCenters++;
+        }
+        return amountOfWorkingCommandCenters >= colonizableAmountOfCommandCenters;
+    }
+}
