ae3c7f5bf36b4a5d037c499092d93d920c950d84
tercsd <tercsd@gmail.com>
2023-01-04 11:33:51 +0100

    include ColonizationTimeoutAnalyzer in ColonizationOutcomeDeterminer

--- Codecool.MarsExploration.MapExplorer/Exploration/Service/ColonizationOutcomeDeterminer.cs
+++ Codecool.MarsExploration.MapExplorer/Exploration/Service/ColonizationOutcomeDeterminer.cs
@@ -6,18 +6,26 @@ namespace Codecool.MarsExploration.MapExplorer.Exploration.Service
     public class ColonizationOutcomeDeterminer : IOutcomeDeterminer
     {
         private readonly IAnalyzer _successAnalyzer;
+        private readonly IAnalyzer _timeoutAnalyzer;
 
         public ColonizationOutcomeDeterminer()
         {
             _successAnalyzer = new ColonizationSuccessAnalyzer();
+            _timeoutAnalyzer = new ColonizationTimeoutAnalyzer();
         }
         public ExplorationOutcome Determine(SimulationContext simulationContext)
         {
             ExplorationOutcome SuccessAnalyzerResult = _successAnalyzer.Analyze(simulationContext);
+            ExplorationOutcome TimeoutAnalyzerResult = _timeoutAnalyzer.Analyze(simulationContext);
 
             if (SuccessAnalyzerResult == ExplorationOutcome.Colonizable)
             {
                 return SuccessAnalyzerResult;
+            }
+
+            if (TimeoutAnalyzerResult == ExplorationOutcome.Timeout)
+            {
+                return TimeoutAnalyzerResult;
             }
 
             return ExplorationOutcome.None;
