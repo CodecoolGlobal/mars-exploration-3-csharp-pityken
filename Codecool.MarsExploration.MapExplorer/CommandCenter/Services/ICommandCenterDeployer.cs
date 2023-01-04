821cc327dc0b177675d02ad7eb6ce855c2c0f132
timigyurina <timi.gyurina@gmail.com>
2023-01-04 16:28:29 +0100

    add ICommandCenterDeployer interface

---
+++ Codecool.MarsExploration.MapExplorer/CommandCenter/Services/ICommandCenterDeployer.cs
@@ -0,0 +1,8 @@
+﻿using Codecool.MarsExploration.MapExplorer.MarsRover.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.CommandCenter.Services;
+
+public interface ICommandCenterDeployer
+{
+    Model.CommandCenter Deploy(Rover rover);
+}
