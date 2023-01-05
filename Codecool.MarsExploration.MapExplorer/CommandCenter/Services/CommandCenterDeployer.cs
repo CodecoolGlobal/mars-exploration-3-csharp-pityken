c2804e8d4504854775686745159a7e1e8e08b8ec
timigyurina <timi.gyurina@gmail.com>
2023-01-05 10:40:53 +0100

    create CommandCenterDeployer class

---
+++ Codecool.MarsExploration.MapExplorer/CommandCenter/Services/CommandCenterDeployer.cs
@@ -0,0 +1,20 @@
+﻿using Codecool.MarsExploration.MapExplorer.MarsRover.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.CommandCenter.Services;
+
+public class CommandCenterDeployer : ICommandCenterDeployer
+{
+    private int _id;
+    private int _;
+
+    public CommandCenterDeployer()
+    {
+        _id = 0;
+    }
+
+    public Model.CommandCenter Deploy(Rover rover)
+    {
+        _id++;
+       // return new Model.CommandCenter(_id, rover.CurrentPosition, );
+    }
+}
